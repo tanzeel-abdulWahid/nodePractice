572216ae1cc592bc344ad98b11e2395a03568b1c
Tanzeel Abdul Wahid <tanzeelwahid111@gmail.com>
2022-08-08 00:44:13 +0500

    till os

---
+++ add.js
@@ -0,0 +1,1 @@
+console.log("Adding two numbers");
---
+++ app.js
@@ -0,0 +1,38 @@
+// const amount = 12;
+
+// if (amount < 10) {
+//     console.log("small amount");
+// }else{
+//     console.log("large amount");
+// }
+
+// console.log(`fist node app`);
+
+// const names = require('./newModule')
+// const sayHi = require('./utils1')
+
+// console.log(names);
+// sayHi(names.name2)
+
+// require('./add.js')
+
+// //! OS Module 
+
+// const os = require('os');
+
+//info about user
+// const user = os.userInfo();
+// console.log(user);
+
+//system uptime in seconds
+
+// console.log(`the sustem uptime in seconds is ${os.uptime()}`);
+
+// const currentOS = {
+//         name: os.type(),
+//         release : os.release(),
+//         totalMem: os.totalmem(),
+//         freeMem: os.freemem(),
+// }
+
+// console.log(currentOS);
