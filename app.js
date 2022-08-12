022cdda28cba6c75b4ee632a0b60c9d82a155bb8
Tanzeel Abdul Wahid <tanzeelwahid111@gmail.com>
2022-08-13 02:14:58 +0500

    get post put dlt methofd

--- app.js
+++ app.js
@@ -1,43 +1,84 @@
 const express = require('express');
 const app = express();
+let {people} = require("./data");
 
-const middleware = module.require('./middleware');
-const authorize = module.require('./authorize');
-// //* res => middleware => res
 
-// //* express ka apna he
-// app.use(express.static('./public'))
+// //*Static assests
+app.use(express.static("./methods-public"))
 
-// //*jo khud bbnae wo custom he 
-// //* morgan library bh he
+// //* parse form data
+app.use(express.urlencoded({extended: false}));
+// //*Parse JSON data
+app.use(express.json());
 
-// app.get('/', middleware, (req, res) => {
-//         res.send("Home");
+// //*GET request
+app.get('/api/people',(req,res) => {
+        res.status(200).json({status:'success',data: people});
+})
+
+// app.get('*',(req,res) => {
+//         res.send("Not found")
 // })
 
-// app.get('/about', middleware,(req, res) => {
-//         res.send("about");
-// });
 
-// //* instead of passing middleware to every get request we do this
-app.use([middleware, authorize])
-// //* we can use multiple middlewares in it by passing in list
+// //* POST req (form ex)
+// app.post("/login",(req,res) => {
+//         const {name} = req.body;
+//         if (name) {
+//                 return res.status(200).send(`Welcome ${name}`);
+//         }
+//         res.status(401).send("Please enter your name");
+// })
 
-// //*if we want to apply middleware specific url we pass path
-// //* it will apply to every url starting with the api
-// app.use('/api',middleware)
-
-app.get('/', (req, res) => {
-        res.send("Home");
+// //* Post ex (javascript https request) 
+app.post('/api/people', (req, res) => {
+        const {name} = req.body;
+        if (!name) {
+                return res.status(400).json({success: false, msg : "Please enter your name"});
+        }
+        res.status(201).json({success: true, person: name});
 })
 
-app.get('/api/items',(req, res) => {
-        console.log(req.user);
-        res.send("Items");
+app.post('/api/postman/people',(req,res) => {
+        const {name} = req.body;
+        if (!name) {
+                return res.status(400).json({success: false, msg : "Please enter your name"});        
+        }
+        res.status(201).json({success: true, data: [...people,name]});
 })
 
+// //*PUT method
+app.put('/api/people/:id',(req,res)=> {
+        const {id} = req.params;
+        const {name} = req.body;
 
-app.listen(5000,()=>{
-        console.log("listening on port");
+        const person = people.find((p) => p.id === Number(id));
+
+        if (!person) {
+                return res.status(404).json({success: false, message: `no person with id ${id}`});
+        }
+        const newPeople = people.map((p) => {
+                if (p.id === Number(id)) {  
+                        p.name = name
+                }
+                return p;
+        });
+        // console.log(person);
+        res.status(200).json({success: true, data : newPeople});
 })
 
+// //* Delete method
+app.delete('/api/people/:id',(req,res) => {
+        const person = people.find((p) => p.id === Number(req.params.id));
+
+        if (!person) {
+                return res.status(404).json({ success: false, message: `no person with id ${req.params.id}` });
+        }
+
+        const newPeople = people.filter((p) => p.id !== Number(req.params.id))
+        return res.status(200).json({success: true, data: newPeople});
+})
+
+app.listen(5000,()=> {
+        console.log('listening on port');
+})
